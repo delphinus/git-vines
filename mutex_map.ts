d785f0d16f9ad6a354630f21c0819ef8536fa8f1
delphinus <me@delphinus.dev>
2021-09-20 23:04:53 +0900

    Use thunk style to simplify logic

--- mutex_map.ts
+++ mutex_map.ts
@@ -4,16 +4,18 @@ export class MutexMap<K, V> {
   private mutex = new Mutex();
   private map = new Map<K, V>();
 
-  async get(k: K): Promise<V | undefined> {
-    const release = await this.mutex.acquire();
-    const v = this.map.get(k);
-    release();
-    return v;
+  get(k: K): Promise<V | undefined> {
+    return this.mutex.use(() =>
+      new Promise((resolve) => resolve(this.map.get(k)))
+    );
   }
 
-  async set(k: K, v: V): Promise<void> {
-    const release = await this.mutex.acquire();
-    this.map.set(k, v);
-    release();
+  set(k: K, v: V): Promise<void> {
+    return this.mutex.use(() =>
+      new Promise((resolve) => {
+        this.map.set(k, v);
+        resolve();
+      })
+    );
   }
 }
